e72a8dc74b1f76763df249fc8ba29ae81c56fbd0
Sam Frengley <sam.frengley@gmail.com>
2024-02-18 15:27:40 +0000

    Added extra tests
    Sanity check analytic Jacobian test in the case when D is > 17 and
    you need the relation in Lambda

--- tests/f_abc_analytic-RM.m
+++ tests/f_abc_analytic-RM.m
@@ -30,3 +30,30 @@ for D in DISCRIMINANTS do
   flag := exists{m : m in min_plys | Discriminant(m) eq D};
   printf "The test for D=%o is %o\n", D, flag;
 end for;
+
+
+/**************************************************
+Sanity checking the equations f_D(a,b,c,z; x);
+**************************************************/
+
+print "\n====================================";
+print "NON-RATIONAL CASES";
+print "TEST: Random choices of a,b,c";
+print "====================================";
+
+DISCRIMINANTS := [21, 28, 29, 37, 44, 53, 61];
+
+for D in DISCRIMINANTS do
+  a,b,c := Explode([Random(-100, 100)/Random(1,50) : i in [1..3]]);
+
+  Lambda := GetUpperLambda_D(D : coords:=[a,b,c]);
+  zeta := Roots(x^2 - Lambda)[1][1];
+
+  f := RMWeierstrassEquationLambda(D, zeta, a, b, c);
+  A := AnalyticJacobian(PP!f);
+  E := EndomorphismAlgebra(A);
+
+  min_plys := [MinimalPolynomial(g) : g in Generators(E)];
+  flag := exists{m : m in min_plys | Discriminant(m) eq D};
+  printf "The test for D=%o is %o\n", D, flag;
+end for;
